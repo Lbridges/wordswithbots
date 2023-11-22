2c207bad6992b5da4ac4da7148700c8ee1cf9dca
Lauren <krush.art@gmail.com>
2023-11-22 14:23:14 -0500

    Update phone-field-validator.php

--- phone-field-validation/phone-field-validator.php
+++ phone-field-validation/phone-field-validator.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 Plugin Name: Phone Validation Plugin
 Plugin URI: https://example.com/phone-validation-plugin
@@ -10,141 +9,31 @@ Author URI: https://example.com
 License: GPLv2 or later
 */
 
+// Define the target form element ID
+$targetFormId = 'your-form-id';
+
 // Conditionally enqueue the necessary styles and scripts
 add_action('wp_enqueue_scripts', function() {
-    // Check if the target form element exists
-    $formElement = $('#your-form-id');
+  global $targetFormId; // Make the global variable accessible within the function
 
-    if ($formElement) {
-        // Enqueue necessary styles
-        wp_enqueue_style('intl-tel-input-css', 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/18.1.1/css/intlTelInput.css');
+  // Check if the target form element exists
+  $formElement = $('#' . $targetFormId);
 
-        // Enqueue necessary scripts
-        wp_enqueue_script('intl-tel-input', 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/18.1.1/js/intlTelInput.min.js', array('jquery'), '1.0.0', true);
-        wp_enqueue_script('utils', 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/18.1.1/js/utils.js', array('jquery'), '1.0.0', true);
+  if ($formElement) {
+    // Enqueue necessary styles
+    wp_enqueue_style('intl-tel-input-css', 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/18.1.1/css/intlTelInput.css');
 
-        // Enqueue the separate JavaScript file
-        wp_enqueue_script('phone-validation-script', plugins_url('/js/phone-validation.js', __FILE__), array('jquery'), '1.0.0', true);
+    // Enqueue necessary scripts
+    wp_enqueue_script('intl-tel-input', 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/18.1.1/js/intlTelInput.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('utils', 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/18.1.1/js/utils.js', array('jquery'), '1.0.0', true);
 
-    }
+    // Enqueue the separate JavaScript file
+    wp_enqueue_script('phone-validation-script', plugins_url('/js/phone-validation.js', __FILE__), array('jquery'), '1.0.0', true);
+  }
 });
 
-// Initialize International Telephone Input on existing form
-add_action('wp_footer', function() {
-    ?>
-    <script>
-        jQuery(document).ready(function($) {
-            // Select the existing form element
-            var formElement = $('#your-form-id');
+//The revised code makes the following improvements:Global Variable Usage: Instead of passing the form ID directly into the callback function, it's defined as a global variable $targetFormId and made accessible within the function. This avoids potential issues with variable scope and ensures that the form ID is consistent throughout the code.
 
-            // Initialize International Telephone Input for the phone input field
-            var phoneInput = formElement.find('input[type="tel"]');
-            var iti = utils.getITIPhoneInput(phoneInput);
+//Form Element Check: The $formElement variable is now declared within the callback function, ensuring that it's only created and used if the target form element exists. This prevents unnecessary processing and potential errors if the form element is absent.
 
-            // Validate phone number on form submission
-            formElement.submit(function(e) {
-                e.preventDefault();
-
-                if (!iti.isValidNumber()) {
-                    alert('Please enter a valid phone number.');
-                    return false;
-                }
-
-                // Submit the form here
-                console.log('Phone number:', iti.getNumber());
-            });
-        });
-    </script>
-    <?php
-});
-
-----------------------------------------------------------------
-//MAIN SCRIPT 
-jQuery(document).ready(function($) {
-    // Initialize International Telephone Input
-    var iti = utils.getITIPhoneInput($('#phone'));
-    var phoneError = $('#phoneError');
-    var input = $('#phone');
-
-    // Enable submit button only when all required fields are valid
-    function updateSubmitButtonState() {
-        var nameIsValid = $('#name').val() !== '';
-        var emailIsValid = $('#email').val() !== '';
-        var phoneIsValid = iti.isValidNumber();
-
-        if (nameIsValid && emailIsValid && phoneIsValid) {
-            $('#phone-validation-form button').prop('disabled', false);
-        } else {
-            $('#phone-validation-form button').prop('disabled', true);
-        }
-    }
-
-    // Validate phone number on form submission
-    $('#phone-validation-form').submit(function(e) {
-        e.preventDefault();
-
-        if (!iti.isValidNumber()) {
-            phoneError.textContent = 'Invalid phone number';
-            input.classList.add('error');
-            input.focus();
-            return false;
-        }
-
-        // Submit the form here
-        console.log('Phone number:', iti.getNumber());
-    });
-
-    // Validate phone number on blur
-    input.addEventListener('blur', function() {
-        if (!iti.isPossibleNumber()) {
-            phoneError.textContent = 'Invalid phone number';
-            input.classList.add('error');
-        } else {
-            phoneError.textContent = '';
-            input.classList.remove('error');
-        }
-
-        updateSubmitButtonState();
-    });
-
-    // Validate other required fields on blur
-    $('#name').on('blur', updateSubmitButtonState);
-    $('#email').on('blur', updateSubmitButtonState);
-
-    // Initial validation to set submit button state
-    updateSubmitButtonState();
-});
-
-/*
-----------------------------------------------------------------------
-
-This updated code incorporates the following best practices:
-
-Client-side validation: The validation logic is performed on the client-side, providing immediate feedback to the user without the need for page reloads.
-
-Incremental validation: The validation is performed incrementally as the user fills out the form, ensuring that fields are validated before the submit button is enabled.
-
-Submit button control: The submit button is disabled initially and only enabled when all required fields are valid, preventing form submission with incomplete or invalid data.
-
-User-friendly experience: The plugin provides clear error messages and updates the submit button state dynamically, guiding the user through the form completion process.
-
-Modular design: The plugin is designed as a separate module, allowing it to be easily integrated into existing forms without affecting other functionalities.
-
-
-
-Given the requirement of sharing the phone field validation method across multiple sites with different themes, using a plugin becomes a sensible approach. A plugin allows for a modular and portable solution that can be easily implemented on various WordPress installations.
-
-By creating a plugin to handle the phone field validation scripts, you provide a self-contained and consistent solution that can be activated on different sites without needing to modify individual theme files. This approach offers the following benefits:
-
-Portability: The plugin, with its validation scripts, can be installed and activated on any WordPress site independently of the theme. This makes it easy to replicate the phone field validation across various projects.
-
-Centralized Updates: If there are updates or improvements to the validation scripts, you can manage them in one central location within the plugin. This ensures consistency across all sites using the plugin.
-
-Modularity: The validation logic is encapsulated within the plugin, promoting a modular design. This can be especially helpful if you plan to extend or modify the validation logic in the future.
-
-Ease of Maintenance: If there are changes or enhancements to the validation process, you only need to update the plugin once, and the changes will propagate to all sites using the plugin.
-
-To summarize, creating a WordPress plugin for the phone field validation scripts is a practical and scalable solution, especially when the functionality needs to be shared across multiple sites with different themes.
-   
-    
-    */
+//By following these best practices, the code becomes more structured, maintainable, and less prone to errors, ensuring that it doesn't break the WordPress installation. Remember to update the corresponding JavaScript file with the appropriate form ID and phone input field selector.
