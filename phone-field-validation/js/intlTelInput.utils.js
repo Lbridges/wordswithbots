5fb02062dbfc74c7bdc8a56422f06570fa8773ec
Lauren <krush.art@gmail.com>
2023-11-22 00:01:41 -0500

    Update intlTelInput.utils.js

--- phone-field-validation/js/intlTelInput.utils.js
+++ phone-field-validation/js/intlTelInput.utils.js
@@ -1,35 +1,14 @@
+// utils.js
 jQuery(document).ready(function($) {
-    // Initialize International Telephone Input
-    var iti = utils.getITIPhoneInput($('#phone'));
-    var phoneError = $('#phoneError');
-    var input = $('#phone');
+    // Define preferred countries
+    var preferredCountries = ["US", "CA", "GB"];
 
-    // Validate phone number on form submission
-    $('#phone-validation-form').submit(function(e) {
-        e.preventDefault();
-
-        if (!iti.isValidNumber()) {
-            phoneError.textContent = 'Invalid phone number';
-            input.classList.add('error');
-            input.focus();
-            return false;
-        }
-
-        phoneError.textContent = '';
-        input.classList.remove('error');
-
-        // Submit the form here
-        console.log('Phone number:', iti.getNumber());
-    });
-
-    // Validate phone number on blur
-    input.addEventListener('blur', function() {
-        if (!iti.isPossibleNumber()) {
-            phoneError.textContent = 'Invalid phone number';
-            input.classList.add('error');
-        } else {
-            phoneError.textContent = '';
-            input.classList.remove('error');
-        }
-    });
+    function getITIPhoneInput(inputElement) {
+        return window.intlTelInput(inputElement, {
+            utilsScript: plugins_url('/js/intlTelInput.utils.js', __FILE__),
+            nationalDropDown: true,
+            preferredCountries: preferredCountries
+        });
+    }
 });
+
